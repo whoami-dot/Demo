3543e56f0d71aab723f2517bcb96b6ca06595e6f
whoami-dot <66989288+whoami-dot@users.noreply.github.com>
2021-02-23 18:02:25 +0630

    Create test-P.py

---
+++ test-P.py
@@ -0,0 +1,23 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+job=input("Enter your job: ")
+
+password=input("Enter password: ")
+
+if password == '12345':
+	print ("""
+	
+	        -----------------------
+	      |                         |
+	      | welcome form our world! |
+	      |                         |
+	        -----------------------
+	
+	""")
+	print ("-"*60)
+	
+	print(
+	
+	"Hello " + name +" your age is "
+	+ age +" ?"
+	)
